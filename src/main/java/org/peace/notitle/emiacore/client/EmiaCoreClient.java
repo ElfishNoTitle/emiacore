316e882452f859c54a424b49902257b0c7c2ae97
NoTitle <elfishnotitle@outlook.com>
2023-07-16 14:29:17 +0800

    基础建设

---
+++ src/main/java/org/peace/notitle/emiacore/client/EmiaCoreClient.java
@@ -0,0 +1,12 @@
+package org.peace.notitle.emiacore.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.MinecraftClient;
+
+public class EmiaCoreClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/org/peace/notitle/emiacore/server/EmiaCoreServer.java
@@ -0,0 +1,10 @@
+package org.peace.notitle.emiacore.server;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class EmiaCoreServer implements DedicatedServerModInitializer {
+    @Override
+    public void onInitializeServer() {
+
+    }
+}
