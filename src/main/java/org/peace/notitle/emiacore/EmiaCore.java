1aeb4ba174ccd4cd2939e09f0fa7164e8b4d982b
NoTitle <elfishnotitle@outlook.com>
2023-07-16 14:26:53 +0800

    基础建设

---
+++ src/main/java/org/peace/notitle/emiacore/EmiaCore.java
@@ -0,0 +1,10 @@
+package org.peace.notitle.emiacore;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EmiaCore implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
